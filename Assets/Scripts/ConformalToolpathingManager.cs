be2a252e8af33701fb29dd3ca6657220efae51fc
Calvin Smith <hyltek@gmail.com>
2025-07-09 16:10:08 -0400

    Made toolpath preview display nan solutions instead of ignoring.

--- Assets/Scripts/ConformalToolpathingManager.cs
+++ Assets/Scripts/ConformalToolpathingManager.cs
@@ -32,6 +32,11 @@ public class ConformalToolpathingManager : MonoBehaviour
 
         // Draw the next segment of the toolpath using FindStepoverPoint.
         Vector2 next_uv = _gcode_generator.FindStepoverPoint(transform.position);
+        // Add it to the line renderer.
+        Vector3 next_point_local = _control_point_obj.CalcBsurface(next_uv.x, next_uv.y);
+        Vector3 next_point_world = _control_point_obj.transform.TransformPoint(next_point_local);
+        _line_renderer.positionCount = _gcode_generator._uv_points.Count + 1; // Set the position count to include the new point.
+        _line_renderer.SetPosition(_line_renderer.positionCount - 1, next_point_world);
 
         // If the next point is NaN, do not add it.
         if (float.IsNaN(next_uv.x) || float.IsNaN(next_uv.y))
@@ -39,11 +44,6 @@ public class ConformalToolpathingManager : MonoBehaviour
             return;
         }
 
-        // Add it to the line renderer.
-        Vector3 next_point_local = _control_point_obj.CalcBsurface(next_uv.x, next_uv.y);
-        Vector3 next_point_world = _control_point_obj.transform.TransformPoint(next_point_local);
-        _line_renderer.positionCount = _gcode_generator._uv_points.Count + 1; // Set the position count to include the new point.
-        _line_renderer.SetPosition(_line_renderer.positionCount - 1, next_point_world);
 
         // If the A button is pressed, add the next point to the uv points.
         bool add_point_button_pressed = OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.RTouch);
