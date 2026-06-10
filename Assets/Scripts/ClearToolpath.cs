cb1645b94db533c57cc2bec023bfecfb0abd7a58
laminha <lamishammouda@hotmail.com>
2026-06-10 13:22:34 -0400

    Adding feedback to clear toolpath button

--- Assets/Scripts/ClearToolpath.cs
+++ Assets/Scripts/ClearToolpath.cs
@@ -1,9 +1,14 @@
 using Oculus.Interaction;
 using UnityEngine;
+using TMPro;
+using System.Collections;
 
 public class ClearToolpath : MonoBehaviour
 {
     public BSurfaceGcodeGenerator _gcode_generator;
+    public TextMeshPro _button_text;
+    public string _default_text = "Clear\nToolpath";
+    public float _feedback_duration = 1f;
 
     void Start()
     {
@@ -14,5 +19,15 @@ public class ClearToolpath : MonoBehaviour
     void Selected(IInteractorView arg)
     {
         _gcode_generator.ClearToolpath();
+        StartCoroutine(ShowClearFeedback());
+    }
+
+    IEnumerator ShowClearFeedback()
+    {
+        _button_text.text = "Clearing...";
+        _button_text.color = Color.green;
+        yield return new WaitForSeconds(_feedback_duration);
+        _button_text.text = _default_text;
+        _button_text.color = Color.white;
     }
 }
